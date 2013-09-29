398544cdb25da7f7e6e68e624b1d8c20e20554e7
Dustin Woo <dustin.woo@outlook.com>
2013-09-29 09:08:35 -0400

    9:08AM saves colour settings, fixed line colour issue

--- js/term.js
+++ js/term.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
         $('#output').append('<div><div class=\"prompt history\">$></div><div class=\"input-line history\">' + 
                             'Welcome! Input "help" for more details.' + '</div></div>');
     }
+    checkForCSS();
     $('.cmdline').keypress(function (e){
         if (e.charCode == 13){
             console.log($('.cmdline').val());
@@ -30,8 +31,15 @@ $(document).ready(function(){
                 clearTerm();
                 localStorage.removeItem('prevTerm');
             } else {
-                $('#output').append('<div><div class=\"prompt history\">$></div><div class=\"input-line history\">'
+                if ($('body').css('background-color') == "rgb(0, 0, 0)") {
+                    $('#output').append('<div style="color: #48E310"><div class=\"prompt history\">$></div><div class=\"input-line history\">'
                             + $('.cmdline').val() + '</div></div>');
+                } else {
+                    $('#output').append('<div style="color: rgb(0,0,0)"><div class=\"prompt history\">$></div><div class=\"input-line history\">'
+                            + $('.cmdline').val() + '</div></div>');
+                }
+                /*$('#output').append('<div class=\"prompt history\">$></div><div class=\"input-line history\">'
+                            + $('.cmdline').val() + '</div></div>');*/
                 if($('.cmdline').val() == 'cc'){
                     changeTermCol();
                 } else {
@@ -64,9 +72,23 @@ function changeTermCol (){
         $('body').css('background-color', 'white');
         $('div').css('color', '#000000');
         $('h2').css('color', '#000000');
+        localStorage.setItem('bodyCSS', 'white');
+        localStorage.setItem('divCSS', '#000000');
+        localStorage.setItem('h2CSS', '#000000');
     } else {
         $('body').css('background-color', 'black');
         $('div').css('color', '#48E310');
         $('h2').css('color', '#48E310');
+        localStorage.setItem('bodyCSS', 'black');
+        localStorage.setItem('divCSS', '#48E310');
+        localStorage.setItem('h2CSS', '#48E310');
+    }
+}
+
+function checkForCSS(){
+    if (localStorage.getItem('bodyCSS') !== null) {
+        $('body').css('background-color', localStorage.getItem('bodyCSS'));
+        $('div').css('color', localStorage.getItem('divCSS'));
+        $('h2').css('color', localStorage.getItem('h2CSS'));
     }
 }
